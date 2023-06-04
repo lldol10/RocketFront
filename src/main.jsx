b7cd0b389c4e9ee778a3a2eb3c9bda43b9d0cb63
lldol10 <luan.lucas@live.com>
2023-06-04 08:38:15 -0300

    downgrade npm

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { ThemeProvider } from 'styled-components'
+import { ThemeProvider } from 'styled-components' 
 
 import {AuthContext, AuthProvider} from './hooks/Auth'
 import GlobalStyles from './styles/global'
